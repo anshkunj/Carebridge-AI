1c6ec99d340d8cf4558c62bf1414bea1ad5f60d1
anshkunj <meghag2075@gmail.com>
2026-02-28 10:59:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -189,5 +189,12 @@ def generate_report():
 
 # -----------------------------
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
